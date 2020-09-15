26724ca205fdb6a652aa26c4d7365a277d8060b4
Musa Rafik <musa_rafik@utexas.edu>
2020-09-14 19:22:42 -0500

    added system design report to Reports

--- frontend/src/pages/Reports.js
+++ frontend/src/pages/Reports.js
@@ -3,13 +3,22 @@ import React from 'react';
 // export const Reports  = () =>(
 const Reports = () =>(
     <div>
-        <h2>Below are our reports for EE364D</h2>
-        <p>Our Project:
-        <a  href="https://docs.google.com/document/d/1ZlsD-6TjKa_ztcnagrnewWZ4UMxnRwC1xuEvjuwLwCE/edit?usp=sharing">Proposal</a>
+        <h2>Below are our reports for our Senior Design Project</h2>
+
+        <p>Our Proposal:
+        <a  href="https://docs.google.com/document/d/1ZlsD-6TjKa_ztcnagrnewWZ4UMxnRwC1xuEvjuwLwCE/edit?usp=sharing"> Proposal</a>
         </p>
+
         <p></p>
+
         <p>Our Project:
-        <a href="https://docs.google.com/document/d/1yLOBlV--6VprLL5PXlqcZhWf08FWEHsnPWvyHlyCTMY/edit?usp=sharing">Definition</a>
+        <a href="https://docs.google.com/document/d/1yLOBlV--6VprLL5PXlqcZhWf08FWEHsnPWvyHlyCTMY/edit?usp=sharing"> Definition</a>
+        </p>
+
+        <p></p>
+
+        <p>Our System Design Report:
+        <a href="https://docs.google.com/document/d/1NnsUV5Lw5Y5NW-nRKLcS6hrLVwCTsCZ_O_ghkilDHQ4/edit?usp=sharing"> System Design Report</a>
         </p>
     </div>
 )
