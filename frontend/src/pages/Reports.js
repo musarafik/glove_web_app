0ce3db989058eb1304ebbdf5225bc9b9ab42a3d0
AnkurKaushik <a123ankur@yahoo.com>
2020-09-19 16:37:43 -0500

    added images to the reports page
    updated with new system report

--- frontend/src/pages/Reports.js
+++ frontend/src/pages/Reports.js
@@ -6,19 +6,29 @@ const Reports = () =>(
         <h2>Below are our reports for our Senior Design Project</h2>
 
         <p>Our Proposal:
+        <img src="/assets/Project_Proposal_img.png"></img>
         <a  href="https://docs.google.com/document/d/1ZlsD-6TjKa_ztcnagrnewWZ4UMxnRwC1xuEvjuwLwCE/edit?usp=sharing"> Proposal</a>
         </p>
 
         <p></p>
 
         <p>Our Project:
+        <img src="/assets/Project_Definition_img.png"></img>
         <a href="https://docs.google.com/document/d/1yLOBlV--6VprLL5PXlqcZhWf08FWEHsnPWvyHlyCTMY/edit?usp=sharing"> Definition</a>
         </p>
 
         <p></p>
 
         <p>Our System Design Report:
+        <img src="/assets/System_Design_Report_img.png"></img>
         <a href="https://docs.google.com/document/d/1NnsUV5Lw5Y5NW-nRKLcS6hrLVwCTsCZ_O_ghkilDHQ4/edit?usp=sharing"> System Design Report</a>
+        </p>
+
+        <p></p>
+
+        <p>Our Updated System Design Report:
+        <img src="/assets/Updated_System_Design_img.png"></img>
+        <a href="https://docs.google.com/document/d/1Ar8vz3wwhaP3VM74Zco3-u2WcxzkhTwbDS4CzAjmT4I/edit?usp=sharing"> System Design Report</a>
         </p>
     </div>
 )
