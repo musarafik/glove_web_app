a170dd53d346434c01a6e0d7075fc499f931cc5b
AnkurKaushik <a123ankur@yahoo.com>
2020-09-23 21:40:11 -0500

    Updated home page, made reports look nicer, added more substance to resources

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,9 +2,21 @@ import React from 'react';
 
 const Home  = () =>(
     <div>
-        <h2>Team I4: Glove</h2>
+        <h2>Team I4: Sign Language Translator Glove</h2>
+        <img src={require('../assets/gl.png')} height="100" width="100" alt="logo"></img> 
         <p>Our main goal is to design a glove capable of translating sign language in real time. We are designing this glove to help people learn sign language by creating an environment where users can test their sign language wherever you can use electronics.</p>
         <p>We are designing this glove to serve as a learning tool for people who wish to learn sign language. To build this glove we need a variety of sensors and a machine learning algorithm. Background Information describes what the various components are and how they work. Whereas, Design Functionality serves to describe how they fit into the glove design. In order to ensure the glove is built with safety in mind, we are holding ourselves to OSHA and ISEA’s professional standards. We are also taking into account ethical considerations involved with the glove such as intellectual property and the health of our users. Finally, we will establish what a successful project consists of by discussing our three deliverables: the electrical components, the algorithm, and the website.</p>
+    
+        <div style={{ display: "grid", gridTemplateColumns: "repeat(2, 1fr)", gridGap: 20 }}>
+          <div>
+            <p> Our Connections</p>
+            <div><img src={require('../assets/connections.png')} height = "512" width = "384" alt=""/></div>   
+          </div>
+          <div>
+            <p> Prototype Glove</p>
+            <div><img src={require('../assets/bglove.png')} alt=""/></div>
+          </div>
+        </div>
     </div>
 )
 
--- frontend/src/pages/Reports.js
+++ frontend/src/pages/Reports.js
@@ -3,33 +3,51 @@ import React from 'react';
 // export const Reports  = () =>(
 const Reports = () =>(
     <div>
-        <h2>Below are our reports for our Senior Design Project</h2>
+        <h2>Senior Design Project Reports</h2>
 
-        <p>Our Proposal:
-        <img src="/assets/Project_Proposal_img.png"></img>
-        <a  href="https://docs.google.com/document/d/1ZlsD-6TjKa_ztcnagrnewWZ4UMxnRwC1xuEvjuwLwCE/edit?usp=sharing"> Proposal</a>
-        </p>
-
-        <p></p>
-
-        <p>Our Project:
-        <img src="/assets/Project_Definition_img.png"></img>
-        <a href="https://docs.google.com/document/d/1yLOBlV--6VprLL5PXlqcZhWf08FWEHsnPWvyHlyCTMY/edit?usp=sharing"> Definition</a>
-        </p>
+        <div style={{ display: "grid", gridTemplateColumns: "repeat(2, 1fr)", gridGap: 20 }}>
+            <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+                <p>Our Project Proposal:  
+                <a  href="https://docs.google.com/document/d/1ZlsD-6TjKa_ztcnagrnewWZ4UMxnRwC1xuEvjuwLwCE/edit?usp=sharing"> Click Here</a>
+                </p>
+            </div>
+            <div><img src={require('../assets/Project_Proposal_img.png')} alt="" height="700"></img></div>
+        </div>
 
         <p></p>
 
-        <p>Our System Design Report:
-        <img src="/assets/System_Design_Report_img.png"></img>
-        <a href="https://docs.google.com/document/d/1NnsUV5Lw5Y5NW-nRKLcS6hrLVwCTsCZ_O_ghkilDHQ4/edit?usp=sharing"> System Design Report</a>
-        </p>
+        <div style={{ display: "grid", gridTemplateColumns: "repeat(2, 1fr)", gridGap: 20 }}>
+            <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+                <p>Our Project Definition:  
+                <a href="https://docs.google.com/document/d/1yLOBlV--6VprLL5PXlqcZhWf08FWEHsnPWvyHlyCTMY/edit?usp=sharing"> Click Here</a>
+                </p>
+            </div>
+            <div><img src={require('../assets/Project_Definition_img.png')} alt="" height="700"></img></div>
+        </div>
 
         <p></p>
 
-        <p>Our Updated System Design Report:
-        <img src="/assets/Updated_System_Design_img.png"></img>
-        <a href="https://docs.google.com/document/d/1Ar8vz3wwhaP3VM74Zco3-u2WcxzkhTwbDS4CzAjmT4I/edit?usp=sharing"> System Design Report</a>
-        </p>
+        
+        <div style={{ display: "grid", gridTemplateColumns: "repeat(2, 1fr)", gridGap: 20 }}>
+            <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+                <p>Our System Design Report:  
+                <a href="https://docs.google.com/document/d/1NnsUV5Lw5Y5NW-nRKLcS6hrLVwCTsCZ_O_ghkilDHQ4/edit?usp=sharing"> Click Here</a>
+                </p>
+            </div>
+            <div><img src={require('../assets/System_Design_Report_img.png')} alt="" height="700"></img></div>
+        </div>
+        
+
+        <p></p>
+
+        <div style={{ display: "grid", gridTemplateColumns: "repeat(2, 1fr)", gridGap: 20 }}>
+            <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+                <p>Our Updated System Design Report:  
+                <a href="https://docs.google.com/document/d/1Ar8vz3wwhaP3VM74Zco3-u2WcxzkhTwbDS4CzAjmT4I/edit?usp=sharing"> Click Here</a>
+                </p>
+            </div>
+            <div><img src={require('../assets/Updated_System_Design_img.png')} alt="" height="700"></img></div>
+        </div>
     </div>
 )
 
