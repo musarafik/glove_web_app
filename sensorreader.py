8456c6112c95d6024815c112f020c0f8c0f1f14d
Morgan Murrell <murrellmorgan@utexas.edu>
2020-11-02 16:24:35 -0600

    bug in sensorreader

--- sensorreader.py
+++ sensorreader.py
@@ -88,6 +88,20 @@ mcp13_p7 = AnalogIn(mcp13, MCP.P7)
 
 # GET IMU VALUES #
 def get_IMU_values():
+	global accel_x_1
+	global accel_y_1
+	global accel_z_1
+	global accel_x_2
+	global accel_y_2
+	global accel_z_2
+
+	global gyro_x_1
+	global gyro_y_1
+	global gyro_z_1
+	global gyro_x_2
+	global gyro_y_2
+	global gyro_z_2
+
 	# IMU READINGS # 
 	accel_x_high_1 = bus.read_byte_data(imu_address_1, 0x2D)
 	accel_x_low_1 = bus.read_byte_data(imu_address_1, 0x2E)
@@ -117,7 +131,9 @@ def get_IMU_values():
 	gyro_z_high_2 = bus.read_byte_data(imu_address_2, 0x37)
 	gyro_z_low_2 = bus.read_byte_data(imu_address_2, 0x38)
 
+
 	# COMBING IMU READING BYTES #
+	accel_x_1 = accel_x_high_1 * 256 + accel_x_low_1
 	accel_y_1 = accel_y_high_1 * 256 + accel_y_low_1
 	accel_z_1 = accel_z_high_1 * 256 + accel_z_low_1
 
